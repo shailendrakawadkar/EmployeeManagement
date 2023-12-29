c1150d5bd7be1936b047cf418c6b6ffc93e0898f
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2023-12-29 16:10:54 +0530

    Add files via upload

--- backend/src/main/java/com/shailendrakawadkar/backend/config/WebConfig.java
+++ backend/src/main/java/com/shailendrakawadkar/backend/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://employee-management-sk.vercel.app/")
                         .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name(), HttpMethod.PUT.name())
                         .allowedHeaders(HttpHeaders.CONTENT_TYPE);
             }
